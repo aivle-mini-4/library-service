b87a218fad4ff0328661bb20b6329a4defd2e4f9
biit han <hb25joo@gmail.com>
2025-07-01 08:51:05 +0000

    포인트관리

--- pointmanagement/src/main/java/aivle/domain/PointExpired.java
+++ pointmanagement/src/main/java/aivle/domain/PointExpired.java
@@ -3,6 +3,7 @@ package aivle.domain;
 import aivle.domain.*;
 import aivle.infra.AbstractEvent;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 import lombok.*;
 
@@ -14,10 +15,14 @@ public class PointExpired extends AbstractEvent {
     private Long id;
     private Long userId;
     private Integer points;
-    private Date history;
+    private LocalDateTime history;
 
     public PointExpired(Point aggregate) {
         super(aggregate);
+        this.id = aggregate.getId();
+        this.userId = aggregate.getUserId();
+        this.points = aggregate.getPoints();
+        this.history = aggregate.getHistory();
     }
 
     public PointExpired() {
---
+++ pointmanagement/src/main/java/aivle/domain/PointType.java
@@ -0,0 +1,7 @@
+package aivle.domain;
+
+public enum PointType {
+    ACCUMULATION,  // 적립
+    USAGE,     // 차감
+    EXPIRATION     // 만료
+}
