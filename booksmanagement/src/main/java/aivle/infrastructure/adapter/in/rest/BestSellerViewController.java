1903296b675f20bcf7c838970793771e006f8f92
leejongsuu <lik9819@naver.com>
2025-07-02 16:47:31 +0900

    refactor: api 경로 수정

--- booksmanagement/src/main/java/aivle/infrastructure/adapter/in/rest/BestSellerViewController.java
+++ booksmanagement/src/main/java/aivle/infrastructure/adapter/in/rest/BestSellerViewController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bestsellers-view")
+@RequestMapping("/bestseller-views")
 @RequiredArgsConstructor
 public class BestSellerViewController {
 
--- booksmanagement/src/main/java/aivle/infrastructure/adapter/in/rest/BookViewController.java
+++ booksmanagement/src/main/java/aivle/infrastructure/adapter/in/rest/BookViewController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/books-view")
+@RequestMapping("/book-views")
 @RequiredArgsConstructor
 public class BookViewController {
 
