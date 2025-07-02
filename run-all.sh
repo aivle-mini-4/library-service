24a75a1a79b4ce32d45647efffcf39eb969d1def
Seonghyeon An <61452538+An-Seonghyeon@users.noreply.github.com>
2025-07-02 12:25:58 +0000

    컨테이너 및 게이트웨이 실행 스크립트 생성

---
+++ run-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker run -d an990906/userinfomanagement:v1
+
+docker run -d an990906/userhistorymanagement:v1
+
+docker run -d an990906/scriptmanagement:v1
+
+docker run -d an990906/pointmanagement:v1
+
+docker run -d an990906/monthlysubscriptionmanagement:v1
+
+docker run -d an990906/booksubscriptionmanagement:v1
+
+docker run -d an990906/booksmanagement:v1
+
+docker run -d an990906/authidentity:v1
+
+docker run -d an990906/aiservice:v1
+
+docker run -d an990906/admintask:v1
+
+
+cd gateway
+mvn spring-boot:run
