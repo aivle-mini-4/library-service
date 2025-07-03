4c2d1e26f3bf450948f608d048142b28c3a616a7
charmeee <mandugohigh@gmail.com>
2025-07-03 12:20:06 +0900

    feat: tailwind 적용

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {worker} from './mocks/browser'
+import './main.css'
+import { worker } from './mocks/browser'
 
 async function prepare() {
   if (import.meta.env.DEV) {
@@ -13,7 +14,7 @@ async function prepare() {
 prepare().then(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <App/>
-    </React.StrictMode>
+      <App />
+    </React.StrictMode>,
   )
 })
