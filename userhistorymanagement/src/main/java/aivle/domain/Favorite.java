6ca403e7ee2d38a598e79cae3f8a6ef98f2c0b09
Seonghyeon An <61452538+An-Seonghyeon@users.noreply.github.com>
2025-07-04 03:09:12 +0000

    즐겨찾기 aggregate 수정

--- userhistorymanagement/src/main/java/aivle/domain/Favorite.java
+++ userhistorymanagement/src/main/java/aivle/domain/Favorite.java
@@ -7,39 +7,49 @@ import lombok.Data;
 import java.util.Date;
 import java.time.LocalDate;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collections;
 
 
 @Entity
-@Table(name = "Favorite_table")
+@Table(name="Favorite_table")
 @Data
 
 //<<< DDD / Aggregate Root
 public class Favorite  {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;    
-       
-    private Long bookId;      
-
+    
+    private Long bookId;    
+    
     private Long userId;
 
-    @PostPersist
-    public void onPostPersist() {
-        FavoriteRegistrerd favoriteRegistrerd = new FavoriteRegistrerd(this);
-        favoriteRegistrerd.publishAfterCommit();
-
-        FavoriteDeleted favoriteDeleted = new FavoriteDeleted(this);
-        favoriteDeleted.publishAfterCommit();
-    }
-
+    
     public static FavoriteRepository repository(){
         FavoriteRepository favoriteRepository = UserhistorymanagementApplication.applicationContext.getBean(FavoriteRepository.class);
         return favoriteRepository;
     }
+
+
+    //<<< Clean Arch / Port Method
+    public void registerFavorite(RegisterFavoriteCommand registerFavoriteCommand){
+        
+        this.userId = registerFavoriteCommand.getUserId();
+        
+        FavoriteRegistrerd favoriteRegistrerd = new FavoriteRegistrerd(this);
+        favoriteRegistrerd.publishAfterCommit();
+    }
+    //>>> Clean Arch / Port Method
+
+    //<<< Clean Arch / Port Method
+    public void deleteFavorite(DeleteFavoriteCommand deleteFavoriteCommand){
+        
+        this.userId = deleteFavoriteCommand.getUserId();
+
+        FavoriteDeleted favoriteDeleted = new FavoriteDeleted(this);
+        favoriteDeleted.publishAfterCommit();
+    }
+    //>>> Clean Arch / Port Method
 }
 //>>> DDD / Aggregate Root
